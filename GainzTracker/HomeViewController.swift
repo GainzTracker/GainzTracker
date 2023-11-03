399a9d47dfa5ec72918cc50d43fce4262facfb02
abigail5536 <142899191+abigail5536@users.noreply.github.com>
2023-11-03 08:31:20 -0400

    Fix home page

--- GainzTracker/HomeViewController.swift
+++ GainzTracker/HomeViewController.swift
@@ -5,4 +5,25 @@
 //  Created by Abigail Johnson on 11/2/23.
 //
 
-import Foundation
+import UIKit
+import ParseSwift
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadUserData()
+    }
+    
+    func loadUserData() {
+        if let currentUser = User.current {
+            usernameLabel.text = currentUser.username
+            emailLabel.text = currentUser.email
+            
+        }
+    }
+}
--- GainzTracker/Models/User.swift
+++ GainzTracker/Models/User.swift
@@ -5,28 +5,23 @@
 //  Created by Abigail Johnson on 10/26/23.
 //
 
-import Foundation
-
+import UIKit
 import ParseSwift
 
-
 struct User: ParseUser {
-    // These are required by `ParseObject`.
     var objectId: String?
     var createdAt: Date?
     var updatedAt: Date?
     var ACL: ParseACL?
     var originalData: Data?
 
-    // These are required by `ParseUser`.
     var username: String?
     var email: String?
     var emailVerified: Bool?
     var password: String?
     var authData: [String: [String: String]?]?
 
-    // Your custom properties.
-
     var lastPostedDate: Date?
-
+    
+    var profileImage: ParseFile?
 }
