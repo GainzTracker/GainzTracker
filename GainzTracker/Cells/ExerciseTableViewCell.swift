c7ec63d89ba79814237edf3e5770081ce7ad60f7
abigail5536 <142899191+abigail5536@users.noreply.github.com>
2023-11-07 15:23:44 -0500

    Worked on diary view controller

---
+++ GainzTracker/Cells/ExerciseTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ExerciseTableViewCell.swift
+//  GainzTracker
+//
+//  Created by Abigail Johnson on 11/7/23.
+//
+
+import UIKit
+
+class ExerciseTableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var setsLabel: UILabel!
+    @IBOutlet weak var repsLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+
+}
+
---
+++ GainzTracker/Learning Tabs/WorkoutDiaryViewController.swift
@@ -0,0 +1,12 @@
+//
+//  WorkoutDiaryViewController.swift
+//  GainzTracker
+//
+//  Created by Abigail Johnson on 11/7/23.
+//
+
+import UIKit
+
+//class WorkoutDiaryViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+//    
+//}
